7ccebd837e0aeef08b6fb17c3ad5b7738b66422d
Zafuzi <zacharyfoutz@gmail.com>
2022-01-17 19:57:02 -0800

    * updated all maps to new style

--- scenes/crusher.gd
+++ scenes/crusher.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
-
+export (int) var delay = 0
+		
+func _process(_delta):
+	if delay > 0:
+		delay -= 1
+		
+	if delay == 0 and $crushTimer.is_stopped():
+		$crushTimer.start()
 
 func _on_crushTimer_timeout():
 	$animation.play("crush")
